d2e0c698d12cae4a10bb2d7361e8029fad9273ce
buzzybounce <jireh.liceralde@gmail.com>
2020-07-13 04:59:07 -0500

    Finished Assignment 4, Part 2 (Models)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 30, message = "Location's name cannot be longer than 30 characters.")
+    private String location;
 
+    public Employer () {}
+
+    public Employer (String location) {
+
+        this.location = location;
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
