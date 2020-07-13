d9c0042b71a308c62eeac9148a1738b851fb684e
buzzybounce <jireh.liceralde@gmail.com>
2020-07-13 05:29:56 -0500

    Finished Assignment 4, Part 3 (Add a jobs Field to Employer)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(max = 30, message = "Location's name cannot be longer than 30 characters.")
     private String location;
+
+    @OneToMany(mappedBy = "test")
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer () {}
 
@@ -25,5 +33,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
